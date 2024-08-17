c8b76bd3b6163e5e3b40ef98162f6b9dc796f293
kamogelomm <kammolawa@gmail.com>
2024-08-17 14:29:42 +0200

    uupdate

--- App.js
+++ App.js
@@ -1,3 +1,64 @@
+//scrollEffect
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".title").forEach((title) => {
+  gsap.fromTo(
+    title,
+    {
+      letterSpacing: "10px",
+      opacity: 0,
+      x: 300,
+      skewX: 1,
+    },
+    {
+      letterSpacing: 0,
+      opacity: 1,
+      x: 0,
+      duration: 1,
+      delay: 0.5,
+      ScrollTrigger: title,
+    }
+  );
+});
+
+gsap.utils.toArray(".p").forEach((p) => {
+  gsap.fromTo(
+    p,
+    {
+      opacity: 0,
+      x: 150,
+      skewX: 10,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      skewX: 0,
+      duration: 1,
+      delay: 0.5,
+      ScrollTrigger: p,
+    }
+  );
+});
+gsap.utils.toArray(".hr").forEach((hr) => {
+  gsap.fromTo(
+    hr,
+    {
+      opacity: 0,
+      x: 0,
+      skewX: 1,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      skewX: 0,
+      duration: 1,
+      delay: 0.5,
+      ScrollTrigger: hr,
+    }
+  );
+});
+
 //hamburger nav button homepage//
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
@@ -11,58 +72,6 @@ document.addEventListener("DOMContentLoaded", () => {
 //type writer effect homepage//
 const typingElement = new Typed(".typing", {
   strings: [" am a Tech Enthusiast."],
-  typeSpeed: 150,
-  backSpeed: 150,
-  looped: true,
-});
-
-let item = document.querySelectorAll(".slider .card");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-
-let active = 0;
-function loadShow() {
-  let stt = 0;
-  item[active].style.transform = `none`;
-  item[active].style.zIndex = 1;
-  item[active].style.filter = "none";
-  item[active].style.opacity = 1;
-
-  for (var i = active + 1; i < item.length; i++) {
-    stt++;
-    item[i].style.transform = `translateX(${120 * stt}px) scale(${
-      1 - 0.2 * stt
-    }) perspective(16px) rotateY(-1deg)`;
-    item[i].style.zIndex = -stt;
-    item[i].style.filter = "blur(5px)";
-    item[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-
-  stt = 0;
-  for (var i = active - 1; i >= 0; i--) {
-    stt++;
-    item[i].style.transform = `translateX(${120 * stt}px) scale(${
-      1 - 0.2 * stt
-    }) perspective(16px) rotateY(1deg)`;
-    item[i].style.zIndex = -stt;
-    item[i].style.filter = "blur(7px)";
-    item[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-}
-loadShow();
-
-next.onclick = function () {
-  active = active + 1 < item.length ? active + 1 : active;
-  loadShow();
-};
-
-prev.onclick = function () {
-  active = active - 1 >= 0 ? active - 1 : active;
-  loadShow();
-};
-
-var typed = new Typed(".autotype", {
-  strings: ["Kamogelo Mmolawa"],
   typeSpeed: 150,
   backSpeed: 150,
   looped: true,
